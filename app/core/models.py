09fa8d03a25a49dfec546c9dd1db76f2b24eaa36
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2020-10-30 09:48:06 -0300

    Added list-recipe feature

--- app/core/models.py
+++ app/core/models.py
@@ -64,16 +64,16 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     """Recipe object"""
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    time_minutes = models.IntegerField()
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
-    title = models.CharField(max_length=255)
-    time_minutes = models.IntegerField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    link = models.CharField(max_length=255, blank=True)
-    ingredients = models.ManyToManyField('Ingredient')
-    tags = models.ManyToManyField('Tag')
 
     def __str__(self):
         return self.title
